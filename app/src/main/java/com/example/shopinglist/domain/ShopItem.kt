b1b94aef5943ee92af33ab08d333ac498c444f18
vsened <vasilysenederov@gmail.com>
2023-03-08 22:52:10 +0500

    Creating presentation layer

--- app/src/main/java/com/example/shopinglist/domain/ShopItem.kt
+++ app/src/main/java/com/example/shopinglist/domain/ShopItem.kt
@@ -3,7 +3,7 @@ package com.example.shopinglist.domain
 data class ShopItem(
     val name: String,
     val count: Int,
-    val enabled: Boolean,
+    var enabled: Boolean,
     var id: Int = UNDEFINED_ID,
 ) {
     companion object {
