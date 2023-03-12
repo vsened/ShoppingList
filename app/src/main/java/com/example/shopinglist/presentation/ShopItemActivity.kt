0286da6fda8d5c3e0d3d7c7b066db9402d27a834
vsened <vasilysenederov@gmail.com>
2023-03-13 02:47:16 +0500

    Add updating adapter data with DiffUtil.ItemCallback

---
+++ app/src/main/java/com/example/shopinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shopinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shopinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
